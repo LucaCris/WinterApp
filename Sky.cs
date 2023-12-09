2382a22244ccae7d103dfdad8cdef99cef70890b
LucaCris <50824617+LucaCris@users.noreply.github.com>
2023-12-09 17:34:01 +0100

    Add project files.

---
+++ Sky.cs
@@ -0,0 +1,57 @@
+﻿using System.Numerics;
+
+namespace WinterApp;
+
+public class Sky
+{
+    Random rnd = new();
+
+    public readonly List<SnowFlake> SFList = [];
+    public float Width { get; private set; }
+    public float Height { get; private set; }
+    public float DPI { get; private set; }
+    public float Floor { get; private set; }
+
+    public void Resize(float width, float height, float dpi)
+    {
+        (Width, Height, DPI) = (width, height, dpi);
+        Floor = height - SnowFlake.Dim;
+        SFList.Clear();
+    }
+
+    public void AddFlake()
+    {
+        SFList.Add(new SnowFlake(rnd.Next(0, (int)Width)));
+    }
+
+    public void Next(float windX)
+    {
+        float windY = Math.Abs(windX) / 4f;
+        foreach (var sf in SFList) {
+            if (sf.IsFalling) {
+                sf.Y += rnd.NextSingle() + windY;
+                sf.X += rnd.NextSingle() - rnd.NextSingle() + windX;
+                if (sf.Y > Floor) {
+                    sf.IsFalling = false;
+                    sf.Y = Floor;
+                    sf.X = (int)((sf.X + SnowFlake.Dim2) / SnowFlake.Dim) * SnowFlake.Dim;
+                }
+                else {
+                    if (SFList.Any(x => !x.IsFalling && Math.Abs(x.X - sf.X) < 1 && Math.Abs(x.Y - sf.Y) < SnowFlake.Dim2))
+                        sf.IsFalling = false;
+                }
+
+                if (sf.X < -SnowFlake.Dim * 2)
+                    sf.X = Width + SnowFlake.Dim;
+                else if (sf.X > Width + SnowFlake.Dim * 2)
+                    sf.X = -SnowFlake.Dim;
+            }
+        }
+
+        int n = SFList.Count(x => !x.IsFalling && x.Y >= Floor);
+        if (n > Width / SnowFlake.Dim) {
+            SFList.RemoveAll(x => !x.IsFalling && x.Y >= Floor);
+            Floor -= SnowFlake.Dim2;
+        }
+    }
+}
---
+++ SnowFlake.cs
@@ -0,0 +1,21 @@
+﻿namespace WinterApp;
+
+public class SnowFlake
+{
+    public const float Dim = 5; 
+    public const float Dim2 = Dim/2;
+    
+    public SnowFlake(float x)
+    {
+        X = x;
+        Y = 0;
+        IsFalling = true;
+        Remove = false;
+    }
+
+    public float X { get; set; }
+    public float Y { get; set; }
+    public bool IsFalling { get; set; }
+    public bool Remove { get; set; }
+
+}
